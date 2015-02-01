322f2d34f09214eb5ff0c503c26d0b10e591b4c0
NikolaBo <NikolaBo@live.com>
2015-02-01 08:39:19 -0800

    implemented hello world
    splash screen test based on toxsick productions tutorial

--- code/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ code/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=MyGdxGame.WIDTH; // sets window width
+        config.height=MyGdxGame.HEIGHT;  // sets window height
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
