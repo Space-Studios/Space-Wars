110db6a5a3cb2c557cf016d0e21775d274474c12
Pop101 <leon.leibmann@gmail.com>
2015-02-06 14:34:36 -0800

    Made game
    I (Leon Leibmann) made the game.

---
+++ code/core/src/com/mygdx/game/objects/ShooterShip.java
@@ -0,0 +1,58 @@
+package com.mygdx.game.objects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+public class ShooterShip {
+	//sets variables
+	Rectangle mask;
+	Sprite sprite;
+	Texture texture;
+	int action;
+	int xVel;
+	int life = 2;
+	//constructor
+	public ShooterShip(){
+		
+	}
+	public void Init(){
+		mask = new Rectangle (0.0f,0.0f,128.0f,64.0f);
+		texture = new Texture(Gdx.files.internal("sprites/Basic Shooter Ship.png"));
+		sprite = new Sprite(texture,0,0,128,64);
+		this.setPlace(0, 0);
+	}
+	//returns if front is colliding
+	public int hits(Rectangle r){
+		if (mask.overlaps(r)){
+		return 1;
+		}
+		else{
+		return -1;
+		}
+	}
+	//action
+	public void action(int type){
+		
+	}
+	//update
+	public void update(float delta){
+		
+	}
+	//sets position of the object
+	public void setPlace(float x,float y){
+		mask.x = x;
+		mask.y = y;
+		sprite.setPosition(x, y);
+	}
+	public void takeDamage(int amount){
+		
+	}
+	public void draw(SpriteBatch batch){
+		sprite.draw(batch);
+	}
+	
+}
+
--- code/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ code/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,8 +7,16 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width=MyGdxGame.WIDTH; // sets window width
-        config.height=MyGdxGame.HEIGHT;  // sets window height
+		
+		//this sets the text of the top of the window (window caption)
+		config.title = "SPACE IS EPIC";
+		//ummmm.. I do not know what this does, It may disable openGL 30. But the video says to do it.
+		config.useGL30=false;
+		//These lines set the width and the height of the window
+		config.width = 1024;
+		config.height = 480;
+		//This line creates the window after all the variables have been set
 		new LwjglApplication(new MyGdxGame(), config);
+		
 	}
 }
