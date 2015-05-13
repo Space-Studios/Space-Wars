fbc90c18db3796342bda57fb5d5179d5103e181a
Pop101 <leon.leibmann@gmail.com>
2015-05-12 18:16:05 -0700

    Balance testing
    Making the game more fun and less money waiting. Also for todays
    performance

--- code/core/src/com/mygdx/game/objects/Constants.java
+++ code/core/src/com/mygdx/game/objects/Constants.java
@@ -13,14 +13,14 @@ public class Constants {
 	public static final int display_height = Gdx.graphics.getDesktopDisplayMode().height;
 	
 	public static final String title = "Space Wars";
-	public static final boolean fullscreen = false;
+	public static final boolean fullscreen = false; //this must stay false
 	//animation stuff
 	public static final int animationSpeed = 3; //this divided by 30 is the frames per second
-	public static final float boomSize = 1;
+	public static final float boomSize = 1.5f;
 	//minor gameplay stuff
 	public static final int shipCool = 30; //if s ship, this value, if sh ship, this value + 10, if b ship, this value *2
 	public static final int moneyCool = 15;
-	public static final int income = 1;
+	public static final int income = 2;
 	//gameplay stuff
 	public static final int sShips = 10;
 	public static final int shShips = 5;
@@ -35,9 +35,9 @@ public class Constants {
 	public static final int shShipDam = 2; //this includes bullet damage
 	public static final int bShipDam = 2;
 	//base stuff
-	public static final int baseLife = 50;
+	public static final int baseLife = 100;
 	//shooter ship shooting cooldown
-	public static final int shotCool = 120;
+	public static final int shotCool = 150;
 	//how long to wait before showing the lose screen
 	public static final int waitBeforeEnd = 200;
 	//how long you sit and stare at the lose screen before the credits pop up and you can stare at something else
