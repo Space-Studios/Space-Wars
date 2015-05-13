ebeb841ab6554aa9b95fdb6b452102d9a94d39f2
Pop101 <leon.leibmann@gmail.com>
2015-05-12 18:45:34 -0700

    Balance testing 2
    number 2!

--- code/core/src/com/mygdx/game/objects/Constants.java
+++ code/core/src/com/mygdx/game/objects/Constants.java
@@ -36,9 +36,9 @@ public class Constants {
 	//ship lives
 	public static final int sShipLife = 1;
 	public static final int shShipLife = 3;
-	public static final int bShipLife = 6;
+	public static final int bShipLife = 8;
 	//ship damage
-	public static final int sShipDam = 5;
+	public static final int sShipDam = 4;
 	public static final int shShipDam = 2; //this includes bullet damage
 	public static final int bShipDam = 2;
 	//base stuff
